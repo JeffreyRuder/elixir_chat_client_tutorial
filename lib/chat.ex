b4b2b34308bd6b43c73836a0468818ccf9719345
Jeff Ruder <JeRuder@fanatics.com>
2019-08-27 10:28:47 -0600

    add start function

--- lib/chat.ex
+++ lib/chat.ex
@@ -1,18 +1,21 @@
 defmodule Chat do
-  @moduledoc """
-  Documentation for Chat.
-  """
+  def start do
+    {host, port} = get_host_and_port()
+    IO.inspect(host)
+    IO.inspect(port)
+  end
 
-  @doc """
-  Hello world.
+  defp get_host_and_port do
+    address = String.trim(IO.gets("Server address (localhost:4000): "))
 
-  ## Examples
+    case address do
+      "" ->
+        {"localhost", 4000}
 
-      iex> Chat.hello()
-      :world
-
-  """
-  def hello do
-    :world
+      _ ->
+        [host, port] = String.split(address, ":")
+        port = String.to_integer(port)
+        {host, port}
+    end
   end
 end
